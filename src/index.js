67aee3249d033424d3d5dacdf8cdb9d4650e54f7
teslo2004 <36759305+teslo2004@users.noreply.github.com>
2020-03-04 00:36:48 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,11 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    var result = "";
+    for (var i = 0; i < expr.length; i += 10){
+        result += MORSE_TABLE[expr.slice(i, i+10).replace(/10/g, ".").replace(/11/g, "-").replace(/00/g, "")] || " ";
+        }
+    return result;
 }
 
 module.exports = {
